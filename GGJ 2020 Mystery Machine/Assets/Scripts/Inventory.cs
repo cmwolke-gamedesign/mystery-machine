6db1ae25a334a2e43e9591e374f313f2afbb41d9
Caroline Wolke <caroline.wolke@accenture.com>
2020-02-01 09:58:18 +0100

    progress

---
+++ GGJ 2020 Mystery Machine/Assets/Scripts/Inventory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public static Inventory Instance;
+    public List<InventorySlot> slots;
+    public GameObject slotPrefab;
+
+    private void Start()
+    {
+        if (Instance != null) {
+            GameObject.Destroy(Instance.gameObject);
+        }
+        Instance = this;
+    }
+
+    public void AddItem(InventoryItem i) {
+        InventorySlot newSlot = GameObject.Instantiate(slotPrefab).GetComponent<InventorySlot>();
+        newSlot.SetItem(i);
+        newSlot.transform.SetParent(transform);
+    }
+
+    public bool RemoveItem(InventoryItem i) {
+        InventorySlot slot = this.slots.Find(s => s.containedItem == i);
+        if (slot != null) {
+            GameObject.Destroy(slot.gameObject);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
