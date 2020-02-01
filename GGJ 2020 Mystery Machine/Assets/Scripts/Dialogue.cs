2cec04b6fbce6f00058da5eacecd0ec0e675982e
Caroline Wolke <caroline.wolke@accenture.com>
2020-02-01 11:57:40 +0100

    ???

--- GGJ 2020 Mystery Machine/Assets/Scripts/Dialogue.cs
+++ GGJ 2020 Mystery Machine/Assets/Scripts/Dialogue.cs
@@ -1,5 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using TMPPro;
+
 
 public class Dialogue : MonoBehaviour {
+
+  public static Dialogue Instance;
+  public TMPro.TextMeshProUGUI characterDialogue;
+  public float dialogueDisplayTime = 3f;
+  public bool talking { private set; get;}
+
+  private Queue<string[]> dialogueQueue;
+
+  private void Start() {
+    if (Instance != null) {
+      Destroy(Instance);
+    }
+    Instance = this;
+    dialogueQueue = new Queue<string[]>();
+  }
+
+
+  private void Update() {
+    if (!talking && dialogueQueue.Peek() != null) {
+      StartCoroutine(Say(dialogueQueue.Dequeue()));
+    }
+  }
+
+  private IEnumerator Say(string[] textSnippets) {
+    this.talking = true;
+    foreach(string nextSnippet in textSnippets) {
+      this.characterDialogue.text = nextSnippet;
+      yield return new WaitForSeconds(dialogueDisplayTime);
+    }
+    yield return new WaitForSeconds(0.5f);
+    this.characterDialogue.text = "";
+    this.talking = false;
+
+  }
+
+  public void SaySomething(string[] bla) {
+     this.dialogueQueue.Enqueue(bla);
+  }
+
+  public void SaySomethingNow(string[] bla) {
+    // override current text ? 
+  }
 }
---
+++ GGJ 2020 Mystery Machine/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private bool isWalking;
+
+    public float movementSpeed = 5f;
+
+    private Vector3 _targetPos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            // layer 9 is "WalkableArea"!
+            print(ray.origin + ", " + ray.direction);
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, 100)) {
+                print(hit.point);
+                _targetPos = new Vector3(hit.point.x, transform.position.y, transform.position.z);
+                isWalking = true;
+            }
+        }
+
+        if (isWalking && Math.Abs(_targetPos.x - transform.position.x) > 1) {
+            transform.position = Vector3.MoveTowards(transform.position, _targetPos, Time.deltaTime * movementSpeed);
+        } else {
+            this.isWalking = false;
+        }
+    }
+}
