97e476508c6db942e88291d1910f0d3d5c5e774c
Caroline Wolke <caroline.wolke@accenture.com>
2020-02-01 23:19:04 +0100

    main scene progress

--- GGJ 2020 Mystery Machine/Assets/Scripts/Interactables/Fridge.cs
+++ GGJ 2020 Mystery Machine/Assets/Scripts/Interactables/Fridge.cs
@@ -8,8 +8,10 @@ public class Fridge : Collectable {
     StartCoroutine(_Collect());
   }
 
+  // say something, fade out screen, open fridge, close fridge ... 
   private IEnumerator _Collect() {
     // todo player anims?
+    Player.Instance.SetPlayerControls(false);
     Dialogue.Instance.SaySomething(new string[] {"This could take a while."});
     yield return new WaitForSeconds(1.5f);
     ScreenEffects.Instance.FadeOutEffect(0.5f, 2f, 0.5f);
@@ -18,7 +20,10 @@ public class Fridge : Collectable {
     transform.parent.Find("scn_kitchen_fridge_open").gameObject.SetActive(true);
     yield return new WaitForSeconds(2f);
     Dialogue.Instance.SaySomething(new string[] {"Finally! Wow, that looks tasty."});
+    yield return new WaitForSeconds(1f);
+    transform.parent.Find("scn_kitchen_fridge_open").gameObject.SetActive(false);
     Inventory.Instance.AddItem(item);
+    Player.Instance.SetPlayerControls(true);
     gameObject.SetActive(false);
   }
 }
