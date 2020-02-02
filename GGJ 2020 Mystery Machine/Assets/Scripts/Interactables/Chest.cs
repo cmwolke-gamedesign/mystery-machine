3cd8fad533445b9f3b68827eef6aa1a678f881a0
Caroline Wolke <caroline.wolke@accenture.com>
2020-02-02 15:25:49 +0100

    bedroom interaction dne.

---
+++ GGJ 2020 Mystery Machine/Assets/Scripts/Interactables/Chest.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+public class Chest : Collectable {
+  
+  public override void Collect() {
+    Inventory.Instance.RemoveItem(requiredItemToCollect);
+    StartCoroutine(_Collect());
+  }
+
+  // say something, fade out screen, open fridge, close fridge ... 
+  private IEnumerator _Collect() {
+    // todo player anims?
+    Player.Instance.SetPlayerControls(false);
+    Dialogue.Instance.SaySomething(new string[] {"I'll open it with the hair pin."});
+    yield return new WaitForSeconds(1.5f);
+    ScreenEffects.Instance.FadeOutEffect(0.5f, 2f, 0.5f);
+    yield return new WaitForSeconds(1f);
+    GetComponent<SpriteRenderer>().enabled = false;
+    yield return new WaitForSeconds(2f);
+    Dialogue.Instance.SaySomething(new string[] {
+      "Ah, the hairpin broke. But I opened it by force.",
+      "There's a beautiful necklace inside. Whose was it?"
+    });
+    yield return new WaitForSeconds(1f);
+    Inventory.Instance.AddItem(item);
+    SoundsManager.Instance.PlaySound(collectSound);
+    Player.Instance.SetPlayerControls(true);
+    gameObject.SetActive(false);
+  }
+}
